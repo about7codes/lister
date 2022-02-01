cee1f4421cc5b8386360bad1e108f6185ad4c148
about7code <sanaullahhaifz@gmail.com>
2022-02-01 17:42:24 +0400

    cors fix

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 const noteRoutes = require('./routes/note');
@@ -12,10 +13,15 @@ require('./helpers/db');
 const app = express();
 const port = process.env.PORT || 3001;
 
+// middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+// app.use(cors());
+
+// routes
 app.use('/user', userRoutes);
 app.use('/notes', auth, noteRoutes);
+
 
 app.get('/', (req, res) => {
     res.send('Hello Lister!');
