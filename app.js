a911be71d3c0211d30955bf1b106d403e68e5585
about7code <sanaullahhaifz@gmail.com>
2022-02-02 16:53:05 +0400

    enabled cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3001;
 // middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 // routes
 app.use('/user', userRoutes);
